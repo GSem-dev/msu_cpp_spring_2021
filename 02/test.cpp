5f80c7a260c54615eff130cce04597f6a6d540dc
GSem-dev <70775668+GSem-dev@users.noreply.github.com>
2021-04-02 18:15:53 +0300

    1

--- 02/test.cpp
+++ 02/test.cpp
@@ -1,14 +1,46 @@
-#include <iostream>
-#include <string>
 #include "parser.hpp"
-#include <algorithm>
 
+void SampleTest()
+{   
+    auto st = [](){std::cout<<"start"<<std::endl;};
+    auto en = [](){std::cout<<"end"<<std::endl;};
+    auto nu = [](unsigned long long n){std::cout<<"Number="<<n<<std::endl;};
+    auto le = [](const std::string& s){std::cout<<"String:"<<s<<std::endl;};
+    
+    TokenParser prs {nu, le, st, en};
+    
+    prs.pars(" Hello \t world 12q 1234");
+}
+void EmptyTest()
+{   
+    auto st = [](){std::cout<<"start"<<std::endl;};
+    auto en = [](){std::cout<<"end"<<std::endl;};
+    auto nu = [](unsigned long long n){std::cout<<"Number="<<n<<std::endl;};
+    auto le = [](const std::string& s){std::cout<<"String:"<<s<<std::endl;};
+    
+    TokenParser prs {nu, le, st, en};
+    
+    prs.pars("");
+    prs.pars(" \t \n ");
+}
 
+void Unit64Test()
+{   
+    auto st = [](){std::cout<<"start"<<std::endl;};
+    auto en = [](){std::cout<<"end"<<std::endl;};
+    auto nu = [](unsigned long long n){std::cout<<"Number="<<n<<std::endl;};
+    auto le = [](const std::string& s){std::cout<<"String:"<<s<<std::endl;};
+    
+    TokenParser prs {nu, le, st, en};
+    
+    prs.pars("18446744073709551617");
+    prs.pars("18446744073709551613");
+}
 
 int main()
 {
-    //SampleTest();
-    //EmptyTest();
-    //Unit64Test();
+    SampleTest();
+    EmptyTest();
+    Unit64Test();
     return 0;
 }
