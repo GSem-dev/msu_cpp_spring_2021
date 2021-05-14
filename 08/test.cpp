836f08ecfe72ef2d3f1846123cb833691c8c6d17
GSem-dev <70775668+GSem-dev@users.noreply.github.com>
2021-05-14 10:32:12 +0300

    первая попытка

--- 08/test.cpp
+++ 08/test.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "pool.h"
+
+#include <thread>
+#include <pthread.h>
+
 struct A {};
 
 void foo(const A&) {}
@@ -7,7 +11,6 @@ void foo(const A&) {}
 int main(int argc, char* argv[])
 {
     ThreadPool pool(8);
-
     auto task1 = pool.exec(foo, A());
     task1.get();
 
